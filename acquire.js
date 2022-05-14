2da33333ac340b5045973b5dc835ea9f3aeb7f46
Lincoln <lgwilkes@gmail.com>
2022-05-13 18:10:06 -0600

    Create player objects

---
+++ acquire.js
@@ -0,0 +1,16 @@
+const player1 = {
+    tiles: [],
+    money: 100,
+    stocks: {
+        echo: 0,
+        bolt: 0,
+    }
+}
+const player2 = {
+    tiles: [],
+    money: 100,
+    stocks: {
+        echo: 0,
+        bot: 0
+    }
+}
