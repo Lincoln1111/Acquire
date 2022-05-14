efb5723180d0bec5138af7d4bb46742641da74f3
Lincoln <lgwilkes@gmail.com>
2022-05-14 13:51:15 -0600

    Add game board

--- acquire.js
+++ acquire.js
@@ -1,3 +1,11 @@
+let gameBoard = [
+    A1, A2, A3, A4, A5,
+    B1, B2, B3, B4, B5,
+]
+/* game board objective: I need to create some sort of table, and the computer pulls random cells and gives them
+to each player. The player then places these "tiles" back on the board, when they're placed next to, above,
+or below each other, magical things happen. 
+*/
 const player1 = {
     tiles: [],
     money: 100,
