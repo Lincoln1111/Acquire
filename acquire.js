ed6266364c781db07989a3c3a183e7f8975c7047
Lincoln <lgwilkes@gmail.com>
2022-05-19 19:14:20 -0600

    Attempt tile ditribution

--- acquire.js
+++ acquire.js
@@ -2,7 +2,7 @@ let gameBoard = [
     ["A1", "A2", "A3", "A4", "A5"],
     ["B1", "B2", "B3", "B4", "B5"]
 ]
-/* game board objective: I need to create some sort of table, and the computer pulls random cells and gives them
+/* game board objective: I need to create some sort of table, that the computer can pull random cells from and give
 to each player. The player then places these "tiles" back on the board, when they're placed next to, above,
 or below each other, magical things happen. 
 */
@@ -22,3 +22,8 @@ const player2 = {
         bot: 0
     }
 }
+player1.tiles.push(gameBoard[Math.floor(Math.random() * gameBoard.length)][Math.floor(Math.random() * gameBoard.length)]);
+// This gives a random tile to the player. But nothing is preventing the computer from distributing that tile again...
+
+console.log(player1);
+console.log(gameBoard);
